07e662ee47fe0bb433b39f5e96db7df2c9296358
Semeon <scherbinasu@gmail.com>
2026-03-12 19:32:18 +0300

    Примеры

--- main/logic/examples/takeoff_land.py
+++ main/logic/examples/takeoff_land.py
@@ -4,6 +4,6 @@ sys.path.append(str(Path(__file__).parent.parent.parent))
 from drone import Point, PID_regulator, Direction, Drone
 drone = Drone()
 drone.start()
-
-
-
+drone.takeoff(1)
+time.sleep(1)
+drone.land()
