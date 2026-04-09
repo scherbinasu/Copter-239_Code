3151f77c287f133d85a524aa16c434e1584b5e46
Semeon <scherbinasu@gmail.com>
2026-04-09 20:17:35 +0300

    база

--- main/old/logic/china_05_2026/logic.py
+++ main/old/logic/china_05_2026/logic.py
@@ -4,6 +4,12 @@ sys.path.append(str(Path(__file__).parent.parent.parent))
 from drone import Point, PID_regulator, Direction, Drone
 drone = Drone()
 drone.start()
+drone.takeoff()
+time.sleep(5)
+scan = drone.get_scan()
+index_scan_sorted_distance = sorted(range(len(scan[2:-2])), key=lambda k: sum([scan[k+i].distance for i in [-2, -1, 0, 1, 2]]))
+
+
 
 
 
